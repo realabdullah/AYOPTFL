289f2bf46334c7ee63eee18d2dd60e1d18631674
Abdullahi Odesanmi <abdulodesanmi@gmail.com>
2023-06-04 11:52:02 +0100

    (chore): mobile menu

--- components/base/Header.vue
+++ components/base/Header.vue
@@ -4,6 +4,8 @@ const props = defineProps<{
 }>();
 
 const color = computed(() => {
+    if (isMenuOpen.value) return '#FFFFFF';
+
     if (props.page === 'home') {
         return '#FFFFFF';
     }
@@ -16,10 +18,26 @@ const isMenuOpen = ref(false);
 const toggleMenu = () => {
     isMenuOpen.value = !isMenuOpen.value;
 };
+
+const mobileMenuStyle = computed(() => {
+    if (isMenuOpen.value) {
+        return {
+            position: 'fixed',
+            zIndex: 2,
+            right: '2rem',
+            left: '2rem',
+        };
+    }
+
+    return {
+       position: 'relative',
+    };
+});
 </script>
 
 <template>
     <div class="header">
+        <!-- DESKTOP -->
         <nav class="header-nav">
             <nuxt-link to="/" class="header-nav__logo">
                 <IconLogo variant="normal" />
@@ -50,9 +68,50 @@ const toggleMenu = () => {
                     </a>
                 </li>
             </ul>
-
-            <IconHamburger :variant="isMenuOpen ? 'close' : 'open'" class="hamburger" @click="toggleMenu" />
         </nav>
+
+        <!-- MOBILE -->
+        <div class="mobile-header" :style="mobileMenuStyle">
+            <nuxt-link to="/" class="mobile-header__logo">
+                <IconLogo variant="normal" />
+            </nuxt-link>
+
+            <button @click="toggleMenu">
+                <IconHamburger :variant="isMenuOpen ? 'close' : 'open'" />
+            </button>
+        </div>
+    </div>
+
+    <div v-show="isMenuOpen" class="mobile">
+        <div class="mobile-nav">
+            <ul>
+                <li><nuxt-link to="/">HOME</nuxt-link></li>
+                <li><nuxt-link to="/">PORTFOLIO</nuxt-link></li>
+                <li><nuxt-link to="/about">ABOUT</nuxt-link></li>
+                <li><nuxt-link to="/contact">CONTACT</nuxt-link></li>
+                <li>
+                    <a href="https://docs.google.com/document/d/1rFh464HNn-HV5wimK8GXVLjyr4nY2crh1t-hto8YIC0/edit"
+                        target="_blank" rel="noopener noreferrer">
+                        <button>Resumé</button>
+                    </a>
+                </li>
+            </ul>
+
+            <div class="socials">
+                <a href="https://www.behance.net/ayoadenowo" target="_blank" rel="noopener noreferrer">
+                    <IconBehance />
+                </a>
+
+                <a href="https://docs.google.com/document/d/1rFh464HNn-HV5wimK8GXVLjyr4nY2crh1t-hto8YIC0/edit"
+                    target="_blank" rel="noopener noreferrer">
+                    <IconLinkedIn />
+                </a>
+
+                <a href="https://twitter.com/AyomideAdenowo1" target="_blank" rel="noopener noreferrer">
+                    <IconTwitter />
+                </a>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -67,6 +126,10 @@ const toggleMenu = () => {
         display: flex;
         align-items: center;
         justify-content: space-between;
+
+        @media (max-width: 800px) {
+            display: none;
+        }
 
         &__logo {
             color: v-bind(color);
@@ -114,13 +177,94 @@ const toggleMenu = () => {
                 }
             }
         }
+    }
 
-        .hamburger {
-            display: none;
+    .mobile-header {
+        display: none;
 
-            @media (max-width: 800px) {
-                display: block;
-                cursor: pointer;
+        @media (max-width: 800px) {
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+            padding: 2.5rem 0;
+        }
+
+        &__logo {
+            color: v-bind(color);
+        }
+
+        button {
+            background: transparent;
+            border: none;
+            cursor: pointer;
+            color: v-bind(color);
+        }
+    }
+}
+
+.mobile {
+    display: block;
+    height: 100%;
+    width: 100%;
+    position: fixed;
+    z-index: 1;
+    left: 0;
+    top: 0;
+    background: $col-navyBlue;
+    transition: 0.5s;
+    overflow-y: auto;
+
+    &-nav {
+        position: absolute;
+        top: 50%;
+        transform: translate(0, -50%);
+
+        ul {
+            display: flex;
+            flex-direction: column;
+            align-items: flex-start;
+            justify-content: center;
+            gap: 2.4rem;
+            padding: 0 2rem;
+
+            li {
+                a {
+                    color: $col-white;
+                    font-weight: 600;
+                    font-size: 4rem;
+                    line-height: 4.8rem;
+                }
+            }
+
+            button {
+                border: 2px solid $col-white;
+                padding: 0.8rem 2.4rem;
+                font-weight: 600;
+                font-size: 1.6rem;
+                line-height: 1.9rem;
+                color: $col-white;
+                background: linear-gradient(to right, $col-white 50%, $col-navyBlue 50%);
+                background-size: 200% 100%;
+                background-position: right bottom;
+                transition: all .5s ease-out;
+
+                &:hover {
+                    cursor: pointer;
+                    background-position: left bottom;
+                    color: $col-navyBlue;
+                }
+            }
+        }
+
+        .socials {
+            display: flex;
+            align-items: center;
+            gap: 4.8rem;
+            padding: 0 2rem;
+            margin-top: 6rem;
+
+            a {
+                color: $col-white;
             }
         }
     }
--- components/icon/Hamburger.vue
+++ components/icon/Hamburger.vue
@@ -7,12 +7,12 @@ defineProps<{
 <template>
     <svg v-if="variant === 'open'" width="24" height="24" viewBox="0 0 24 24" fill="none"
         xmlns="http://www.w3.org/2000/svg">
-        <path d="M2 4H22V6.5H2V4ZM7 10.25H22V12.75H7V10.25ZM13.25 16.5H22V19H13.25V16.5Z" fill="white" />
+        <path d="M2 4H22V6.5H2V4ZM7 10.25H22V12.75H7V10.25ZM13.25 16.5H22V19H13.25V16.5Z" fill="currentColor" />
     </svg>
 
     <svg v-else-if="variant === 'close'" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path
             d="M5.15385 19L4 17.8462L10.3462 11.5L4 5.15385L5.15385 4L11.5 10.3462L17.8462 4L19 5.15385L12.6538 11.5L19 17.8462L17.8462 19L11.5 12.6538L5.15385 19Z"
-            fill="white" />
+            fill="currentColor" />
     </svg>
 </template>
